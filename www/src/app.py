b74c895803b140a8615e0f7b0d1005e60bff0731
kin <zhangjing@iappease.com.cn>
2017-04-25 14:56:17 +0800

    update

--- www/src/app.py
+++ www/src/app.py
@@ -1,20 +1,12 @@
-import logging; logging.basicConfig(level=logging.INFO)
-import asyncio, os, json, time
-from datetime import datetime
-from aiohttp import web
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hi iFer!</h1>'
 
 
-def index(request):
-    return web.Response(body='<h1>Awesome</h1>', content_type='text/html')
 
-@asyncio.coroutine
-async def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
-    return srv
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+if __name__ == '__main__':
+    app.run(debug=True)
